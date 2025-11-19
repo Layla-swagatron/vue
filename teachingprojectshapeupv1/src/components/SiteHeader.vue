22320ae175c16096ef2ee7b171df77458736a20d
Layla Horan <k00324052@student.tus.ie>
2025-11-19 15:37:12 +0000

    site header commit

---
+++ teachingprojectshapeupv1/src/components/SiteHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <img src="/images/shape_up_logo.png" alt="Shape Up logo" />
+    <div>
+      <h2>Shape Up!</h2>
+      <h3>Find the best fit for you</h3>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
