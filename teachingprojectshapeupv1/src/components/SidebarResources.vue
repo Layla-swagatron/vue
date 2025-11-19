c615ba5c5be03f1cec7ba6ec969491aca3bede84
Layla Horan <k00324052@student.tus.ie>
2025-11-19 15:37:32 +0000

    side bar resoucres commit

---
+++ teachingprojectshapeupv1/src/components/SidebarResources.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<script setup>
+const links = [
+  { href: "https://www.cdc.gov/physicalactivity/basics/pa-health/index.htm", text: "Physical activity and health" },
+  { href: "http://www.heart.org/HEARTORG/HealthyLiving/PhysicalActivity/FitnessBasics/FlexibilityExercise-Stretching_UCM_464002_Article.jsp#.WpB8zIPwaUl", text: "Stretching exercises" },
+  { href: "http://www.heart.org/HEARTORG/HealthyLiving/PhysicalActivity/FitnessBasics/Strengthand-Resistance-Training-Exercise_UCM_462357_Article.jsp#.WpCPxIPwaUl", text: "Strengthtraining" },
+  { href: "https://www.health.harvard.edu/exercise-and-fitness/interval-training-for-a-strongerheart", text: "Cardiovascular training" },
+  { href: "https://www.self.com/gallery/power-yoga-workout", text: "Yoga workout" }
+]
+</script>
+<template>
+  <div>
+    <h2>Additional resources</h2>
+    <ul>
+      <li v-for="link in links" :key="link.href">
+        <a :href="link.href" target="_blank" rel="noopener noreferrer">{{ link.text }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
